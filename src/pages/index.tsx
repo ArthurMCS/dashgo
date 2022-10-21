7b231c49726f35f640555ad6460dc8f1d95f1d06
Arthur Moreira <arthurmcs96@gmail.com>
2022-10-21 14:00:02 -0300

    componentiza os inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Button, Flex, Stack } from '@chakra-ui/react'
 import React from 'react'
+import Input from './components/form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
    <Flex
      w="100vw"
@@ -20,42 +21,16 @@ export default function Home() {
       >
 
        <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">
-              E-mail
-            </FormLabel>
-
-            <Input
-                name='email'
-                id="email"
-                type="email"
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
-                _hover={{
-                  bgColor: 'gray.900'
-                }}
-                size="lg"
-              />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">
-              Senha
-            </FormLabel>
-
-            <Input
-              name="password"
-              id="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: "gray.900"
-              }}
-              size="lg"
-            />
-          </FormControl>      
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+          />
+           <Input
+            name="senha"
+            type="password"
+            label="Senha"
+          />     
        </Stack>
 
         <Button
