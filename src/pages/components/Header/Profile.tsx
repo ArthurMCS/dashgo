2f5f9fd0d0d92907f9809a503d5075d31612f003
Arthur Moreira <arthurmcs96@gmail.com>
2022-10-25 14:19:39 -0300

    works the responsiveness of the header component

--- src/pages/components/Header/Profile.tsx
+++ src/pages/components/Header/Profile.tsx
@@ -1,17 +1,23 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
-export default function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true}: ProfileProps) {
   return (
    <Flex align="center">
-   <Box mr="4" textAlign="right">
-       <Text>Arthur Moreira</Text>
-       <Text
-           color="gray.300" fontSize="small"
-       >
-           arthurmoreiradev@gmail.com
-       </Text>
-   </Box>
+    {showProfileData && (
+            <Box mr="4" textAlign="right">
+                <Text>Arthur Moreira</Text>
+                <Text
+                    color="gray.300" fontSize="small"
+                >
+                    arthurmoreiradev@gmail.com
+                </Text>
+            </Box>
+    )}
        <Avatar 
            size="md"
            name="Arthur Moreira"
